1d3842fe2d7ae8c8022e4b873f87be6e80cff235
wajihma <wajiho.mahrouseh@gmail.com>
2022-04-23 15:41:44 +0300

    Generic-DetailView

--- gcbv/urls.py
+++ gcbv/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns = [
     path('make_list/', views.MakeList.as_view()),
     path('auto_list/', views.AutoList.as_view()),
+
+    path('make_detail/<int:pk>/', views.MakeDetail.as_view()),
+    path('auto_detail/<int:pks>/', views.AutoDetail.as_view()),
+    path('auto_detail/<slug:slugify>/', views.AutoDetail.as_view()),
+
 ]
--- gcbv/views.py
+++ gcbv/views.py
@@ -34,3 +34,36 @@ class AutoList(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.order_by('nickname')
+
+
+# detail view
+class MakeDetail(DetailView):
+    model = Make
+
+
+class AutoDetail(DetailView):
+    model = Auto
+    # queryset = Auto.objects.all()
+    context_object_name = 'aut'
+    pk_url_kwarg = 'pks'
+    slug_field = 'nickname'
+    slug_url_kwarg = 'slugify'
+    query_pk_and_slug = True
+    template_name = 'gcbv/auto_detail_v.html'
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['zxc'] = 'Hello form get_context_data method!'
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # return queryset.filter(make__name='BMW')
+        return queryset
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.mileage = obj.mileage + 1
+        obj.save()
+        return obj
